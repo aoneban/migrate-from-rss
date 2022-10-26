50f8a1cac9cee2df80700a2486a63f4048c55d4f
aoneban <torgregion1@gmail.com>
2022-10-26 18:11:00 +0200

    create: add feed a frend

--- online-zoo/pages/donate/index.js
+++ online-zoo/pages/donate/index.js
@@ -1,9 +1,27 @@
 let inputs = document.querySelectorAll(".input__donate__2");
+const sum = document.querySelectorAll(".choise__sum");
+const dollarsBlack = document.querySelectorAll('.dollar-black')
+const dollarsGold = document.querySelectorAll('.dollar-gold')
+sum[2].style.color = "orange";
+dollarsBlack[2].style.display = 'none'
+dollarsGold[2].style.display = 'inline'
+console.log(dollarsGold)
 
 inputs.forEach((el) => {
   el.addEventListener("input", (e) => {
-    let newData = document.querySelector(".anoteher__amount").value;
     let newTarget = e.currentTarget.value;
     document.querySelector(".anoteher__amount").value = newTarget;
+    for (let i = 0; i < sum.length; i++) {
+      if (sum[i].innerText == newTarget) {
+        sum[i].style.color = "orange";
+        dollarsBlack[i].style.display = 'none'
+        dollarsGold[i].style.display = 'inline'
+      } else {
+        sum[i].style.color = "#555";
+        dollarsBlack[i].style.display = 'inline'
+        dollarsGold[i].style.display = 'none'
+
+      }
+    }
   });
 });
