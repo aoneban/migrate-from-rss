97478765499bf74ba580b193590f17e4d24b921e
aoneban <torgregion1@gmail.com>
2022-11-17 16:24:07 +0100

    fix: correct and upgrade webpack

--- src/calc.js
+++
@@ -1,2 +0,0 @@
-export const myCalc = (a , b) => {a + b}
-
--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-import { myCalc } from "./calc.js";
+import "./index.html";
+import "./index.scss";
+import code from "./img/code.png";
+import { mult, sum } from "./modules/calc";
 
-myCalc(5, 7);
+const imgWrap = document.querySelector(".img");
+const img = new Image();
+img.src = code;
+img.width = 400;
+imgWrap.append(img);
+
+
+console.log(mult(3, 4));
+console.log(sum(3, 4));
