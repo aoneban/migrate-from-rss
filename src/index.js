3c5c77ae75ecf0420b514af010185da060c5b07e
aoneban <torgregion1@gmail.com>
2022-11-19 15:34:26 +0100

    feat: the function of creating a player and adding a random song to it

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ import "./index.html";
 import "./index.scss";
 import code from "./img/code.jpg";
 import birdsData from "./modules/data";
-import { mult, sum } from "./modules/calc";
+import {
+  birdsNames,
+  setBirds,
+  randomInteger,
+  randomSongGenerator,
+  generatePlayerWithSong,
+} from "./modules/functions";
 
 const imgWrap = document.querySelector(".img");
 const img = new Image();
@@ -10,30 +16,19 @@ img.src = code;
 img.width = 200;
 imgWrap.append(img);
 
-console.log(mult(3, 4));
-console.log(sum(3, 4));
+setBirds(birdsNames(birdsData, 0));
+generatePlayerWithSong();
 
-//array of passerine birds
+const newArr = document.getElementsByClassName("data-birds");
+console.log(newArr);
 
-function myBirds(arr, num) {
-  let newBirds = [];
-  for (let i = 0; i < 6; i++) {
-    newBirds.push(arr[num][i]);
-  }
-  return newBirds.map((el) => el.name);
+for (let item of newArr) {
+  item.addEventListener("click", (e) => {
+    if (e.target !== e.currentTarget) {
+      e.stopPropagation();
+    } else {
+      alert('hi');
+      console.log(e.currentTarget.innerText)
+    }
+  });
 }
-
-//list of birds
-
-function setBirds(data) {
-  const list = document.querySelector('.wrapper-list')
-  document.querySelector(".wrapper-list").innerHTML = "";
-
-    data.forEach((_,ind) => {
-    const listEl = document.createElement("li");
-    listEl.innerHTML = `<span class="dot"></span>${data[ind]}`
-    list.appendChild(listEl)   
-    })
-
-}
-setBirds(myBirds(birdsData, 0));
---
+++ src/modules/functions.js
@@ -0,0 +1,62 @@
+import birdsData from "./data";
+
+//array of passerine birds
+//создан массив имен птиц семейства воробьиных
+
+export function birdsNames(arr, num) {
+  let newBirds = [];
+  for (let i = 0; i < 6; i++) {
+    newBirds.push(arr[num][i]);
+  }
+  return newBirds.map((el) => el.name);
+}
+
+//list of birds
+//вывод массива имен сеймейства воробьиных
+
+export function setBirds(data) {
+  const list = document.querySelector(".wrapper-list");
+  document.querySelector(".wrapper-list").innerHTML = "";
+
+  data.forEach((_, ind) => {
+    const listEl = document.createElement("li");
+    listEl.classList.add('data-birds')
+    listEl.innerHTML = `<span class="dot"></span>${data[ind]}`;
+    list.appendChild(listEl);
+  });
+}
+
+// random number generation to generate random song
+ // генератор случайного числа для рандмного выбора песни
+export function randomInteger(min, max) {
+  let rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+}
+
+// generate link to random song
+// генератор ссылки случайной песни
+export function randomSongGenerator(arr, num) {
+  let newSong = [];
+  for (let i = 0; i < 6; i++) {
+    newSong.push(arr[num][i]);
+  }
+  newSong = newSong.map((el) => el.audio);
+  return newSong[randomInteger(1, 5)]
+}
+
+// функция создания плеера и добавления в него рандомной песни
+//the function of creating a player and adding a random song to it
+export function generatePlayerWithSong() {
+  const list = document.querySelector(".content-bird");
+  document.querySelector(".content-bird").innerHTML = "";
+
+  const h3 = document.createElement('h3')
+  h3.textContent = '*****'
+  list.appendChild(h3)
+  const audio = document.createElement('div')
+  audio.innerHTML = `<audio controls>
+  <source src="${randomSongGenerator(birdsData, 0)}" type="audio/mp3">
+  <source src="${randomSongGenerator(birdsData, 0)}" type="audio/mp3">
+</audio>`
+  list.appendChild(audio)
+}
