e120364139866e736bf95db843f52b370e8ea4b2
aoneban <torgregion1@gmail.com>
2022-12-10 00:10:05 +0100

    feat: add API-key

--- News-API/src/components/controller/appLoader.js
+++ News-API/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '0e6db3dc33254ba3b97c126cb33f517e', // получите свой ключ https://newsapi.org/
         });
     }
 }
