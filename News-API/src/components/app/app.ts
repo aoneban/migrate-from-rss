82056f6ed771e728cfffa11e48291a0fe60032bd
aoneban <torgregion1@gmail.com>
2022-12-11 19:57:49 +0100

    feat: add migration app.ts

--- News-API/src/components/app/app.ts
+++ News-API/src/components/app/app.ts
@@ -1,7 +1,10 @@
 import AppController from '../controller/controller';
 import { AppView } from '../view/appView';
+import { IItemsData } from '../../types/index';
 
 class App {
+    controller: AppController;
+    view: AppView;
     constructor() {
         this.controller = new AppController();
         this.view = new AppView();
@@ -10,7 +13,7 @@ class App {
     start() {
         document
             .querySelector('.sources')
-            .addEventListener('click', (e) => this.controller.getNews(e, (data) => this.view.drawNews(data)));
+            .addEventListener('click', (e) => this.controller.getNews(e, (data: IItemsData) => this.view.drawNews(data)));
         this.controller.getSources((data) => this.view.drawSources(data));
     }
 }
