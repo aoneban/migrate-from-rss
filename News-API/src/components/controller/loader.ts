bdf30686afe4101b7a63b62d90793b0dab6135d8
aoneban <torgregion1@gmail.com>
2022-12-11 19:27:56 +0100

    feat: add migration loader.ts

--- News-API/src/components/controller/loader.ts
+++ News-API/src/components/controller/loader.ts
@@ -1,5 +1,11 @@
+interface Option {
+    apiKey: string;
+}
+
 class Loader {
-    constructor(baseLink, options) {
+    baseLink: string;
+    options: Option;
+    constructor(baseLink: string, options: Option) {
         this.baseLink = baseLink;
         this.options = options;
     }
@@ -13,7 +19,9 @@ class Loader {
         this.load('GET', endpoint, callback, options);
     }
 
-    errorHandler(res) {
+    errorHandler(res: {
+        json(): any; ok: string; status: number; statusText: string | undefined; 
+}) {
         if (!res.ok) {
             if (res.status === 401 || res.status === 404)
                 console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
@@ -23,8 +31,8 @@ class Loader {
         return res;
     }
 
-    makeUrl(options, endpoint) {
-        const urlOptions = { ...this.options, ...options };
+    makeUrl(options: string [], endpoint: string) {
+        const urlOptions: number | string | undefined | string[] | ConcatArray<string>[] = { ...this.options, ...options };
         let url = `${this.baseLink}${endpoint}?`;
 
         Object.keys(urlOptions).forEach((key) => {
@@ -34,7 +42,7 @@ class Loader {
         return url.slice(0, -1);
     }
 
-    load(method, endpoint, callback, options = {}) {
+    load(method: string, endpoint: string, callback: { (): void; (arg0: string): string; }, options = {}) {     
         fetch(this.makeUrl(options, endpoint), { method })
             .then(this.errorHandler)
             .then((res) => res.json())
