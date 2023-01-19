9aaa7394f168dee0a6e25abfd6a3bb7d3c92b97c
aoneban <torgregion1@gmail.com>
2023-01-19 20:32:30 +0100

    init: start async-race-task

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import './style.scss';
+
+const root = document.getElementById('root');
+const img = document.createElement('img');
+img.src = './image.jpg';
+root?.append(img);
+
+console.log('Hello world');
