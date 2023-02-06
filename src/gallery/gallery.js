4e6d339a98e425a705416ae875ea5c191a99d888
aoneban <torgregion1@gmail.com>
2023-02-06 12:50:30 +0100

    refactor: replaced html code in gallery

--- src/gallery/gallery.js
+++ src/gallery/gallery.js
@@ -1,33 +1,41 @@
-import birdsDataEn from "../modules/dataEn";
+import birdsDataEn from '../modules/dataEn';
 
-
-showBirds(birdsDataEn, 0, 1)
+showBirds(birdsDataEn, 0, 1);
 function showBirds(data, x, num) {
-    const wrapper = document.querySelector(".wrapper");
-    for(let i = x; i < num; i++) {
-        data[i].forEach(el => {
-            const birdElement = document.createElement("div")
-            birdElement.classList.add("element")
-            birdElement.innerHTML = `
-              <img class="element-img" src=${el.image} alt="bird" />
-              <div class="color item-name">${el.name}</div>
-              <div class="item-latino">${el.species}</div>
-              <audio class="new-audio" controls>
-                <source src=${el.audio} type="audio/mp3">
-                Your browser does not support the audio element.
-              </audio>
-              <div class="color item-desc">${el.description}</div>
-            `
-            wrapper.appendChild(birdElement)
-        });
-    }   
+  const wrapper = document.querySelector('.wrapper');
+  for (let i = x; i < num; i++) {
+    data[i].forEach((el) => {
+      const birdElement = document.createElement('div');
+      birdElement.classList.add('element');
+      const img = document.createElement('img');
+      img.classList.add('element-img');
+      img.src = el.image;
+      img.alt = el.name;
+      const item = document.createElement('div');
+      item.classList.add('color', 'item-name');
+      item.textContent = el.name;
+      const itemLatino = document.createElement('div');
+      itemLatino.classList.add('item-latino');
+      itemLatino.textContent = el.species;
+      const audio = document.createElement('audio');
+      audio.setAttribute('type', 'audio/mp3');
+      audio.setAttribute('src', el.audio);
+      audio.setAttribute('controls', 'controls');
+      audio.classList.add('new-audio');
+      const itemDesc = document.createElement('div');
+      itemDesc.classList.add('color', 'item-desc');
+      itemDesc.textContent = el.description;
+
+      birdElement.append(img, item, itemLatino, audio, itemDesc)
+      wrapper.appendChild(birdElement);
+    });
+  }
 }
 
-const btn2 = document.getElementById("btn")
-btn2.addEventListener("click",() => newFunc(1, 6))
+const btn2 = document.getElementById('btn');
+btn2.addEventListener('click', () => newFunc(1, 6));
 
 function newFunc(x, arg) {
-    showBirds(birdsDataEn, x, arg)
-    btn2.disabled = true
+  showBirds(birdsDataEn, x, arg);
+  btn2.disabled = true;
 }
-
